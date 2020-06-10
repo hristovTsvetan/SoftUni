c19a58bc0d310e327ec5e237183d0f5566322a4a
unknown <tsvetan.hristov@tsvetan.eu>
2020-06-10 14:55:25 +0300

    Homework

---
+++ c#/C# Advanced/Streams Files And Directories/ExTask01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FileAndStreamsEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Path.Combine("files", "input.txt");
+            var fr = new StreamReader(path);
+            string[] charForReplace = new string[] { "-", ",", ".", "!", "?" };
+
+            int rowCounter = 0;
+            while(!fr.EndOfStream)
+            {
+                string line = fr.ReadLine();
+                if(rowCounter%2 == 0)
+                {
+                    foreach (var character in charForReplace)
+                    {
+                        if(line.Contains(character))
+                        {
+                            line = line.Replace(character, "@");
+                        }
+                    }
+                    string[] tempArr = line.Split(" ").Reverse().ToArray();
+                    line = string.Join(" ", tempArr);
+                    Console.WriteLine(line);
+                }
+                rowCounter++;
+            }
+        }
+    }
+}
