f532659e040656061e10e75f0ebb5c5e74b55626
unknown <tsvetan.hristov@tsvetan.eu>
2020-07-28 16:36:36 +0300

    exercises

---
+++ c#/C# OOP/Reflection Attributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private readonly int minValue;
+        private readonly int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            CheckRange(minValue, maxValue);
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if(obj is Int32)
+            {
+                int curValue = (int)obj;
+                if(curValue < minValue || curValue > maxValue)
+                {
+                    return false;
+                }
+                return true;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid type!");
+            }
+        }
+
+        public void CheckRange(int minValue, int maxValue)
+        {
+            if(minValue > maxValue)
+            {
+                throw new ArgumentException("Range is not valid!");
+            }
+        }
+    }
+}
---
+++ c#/C# OOP/Reflection Attributes/Utilities/Validator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utilities
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            if(obj == null)
+            {
+                 throw new ArgumentException("Object can't be null!");
+            }
+
+            Type objType = obj.GetType();
+            PropertyInfo[] allProperties = objType.GetProperties();
+
+            foreach(PropertyInfo property in allProperties)
+            {
+                MyValidationAttribute[] allAttributes = property.GetCustomAttributes()
+                    .Where(attr => attr is MyValidationAttribute)
+                    .Cast<MyValidationAttribute>()
+                    .ToArray();
+
+                foreach (MyValidationAttribute attribute in allAttributes)
+                {
+                    if(!attribute.IsValid(property.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
